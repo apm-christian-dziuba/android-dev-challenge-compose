fd3a046baf1c4390a2be4b724bb1200148da1fc7
christian dziuba <christian@appmotion.de>
2021-02-26 16:05:59 +0100

    feat(list): add puppy list screen

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -16,13 +16,30 @@
 package com.example.androiddevchallenge
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
@@ -39,9 +56,15 @@ class MainActivity : AppCompatActivity() {
 // Start building your app here!
 @Composable
 fun MyApp() {
+    val puppyList = MockDataGenerator().generatePuppyList()
+
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        PuppyList(puppies = puppyList, modifier = Modifier.fillMaxWidth(), onClick = { puppy -> onPuppyClicked(puppy = puppy) })
     }
+}
+
+fun onPuppyClicked(puppy: Puppy) {
+    Log.d("MainActivity", "puppy clicked: ${puppy.name}")
 }
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
@@ -57,5 +80,39 @@ fun LightPreview() {
 fun DarkPreview() {
     MyTheme(darkTheme = true) {
         MyApp()
+    }
+}
+
+@Composable
+fun PuppyList(puppies: List<Puppy>, modifier: Modifier, onClick: (puppy: Puppy) -> Unit) {
+    LazyColumn(verticalArrangement = Arrangement.spacedBy(16.dp), contentPadding = PaddingValues(16.dp), modifier = Modifier.fillMaxWidth()) {
+        items(puppies) { puppy ->
+            PuppyListItem(puppy = puppy, modifier = modifier, onClick = onClick)
+        }
+    }
+}
+
+@Composable
+fun PuppyListItem(puppy: Puppy, modifier: Modifier = Modifier, onClick: (puppy: Puppy) -> Unit) {
+    Card(elevation = 8.dp, modifier = modifier.clickable { onClick(puppy) }) {
+        Column {
+            Image(
+                painter = painterResource(id = puppy.imageResourceId),
+                contentDescription = "Puppy Image",
+                modifier = Modifier.height(200.dp),
+                contentScale = ContentScale.Crop
+            )
+            Row(modifier = Modifier.padding(12.dp)) {
+                Text(
+                    text = when (puppy.type) {
+                        PuppyType.CAT -> "🐱"
+                        PuppyType.DOG -> "\uD83D\uDC15"
+                        else -> "\uD83D\uDC7D"
+                    },
+                    style = MaterialTheme.typography.h5
+                )
+                Text(text = puppy.name, style = MaterialTheme.typography.h5, modifier = Modifier.padding(horizontal = 16.dp))
+            }
+        }
     }
 }
